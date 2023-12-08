6b3368db05e3b5b56c7592bcdabca135cf550ea1
devmarcosav@gmail.com <devmarcosav@gmail.com>
2023-12-08 17:24:06 -0300

    feat: create query for sqlc

---
+++ db/query/user.sql
@@ -0,0 +1,28 @@
+-- name: CreateUser :one
+INSERT INTO users (
+    username,
+    password,
+    email
+) VALUES (
+    $1, $2, $3
+) RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users WHERE username = $1 LIMIT 1;
+
+-- name: GetUserById :one
+SELECT * FROM users WHERE id = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE users SET
+    username = COALESCE($1, username),
+    password = COALESCE($2, password),
+    email = COALESCE($3, email)
+WHERE id = $4
+RETURNING *;
+
+-- name: DeleteUserID :exec
+DELETE FROM users WHERE id = $1;
+
+-- name: DeleteUserUsername :exec
+DELETE FROM users WHERE username = $1;
