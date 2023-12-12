28744be22be3b291e4299439a80e3279c66012eb
devmarcosav@gmail.com <devmarcosav@gmail.com>
2023-12-12 11:06:38 -0300

    feat: create security hash and token

---
+++ security/auth-token.go
@@ -0,0 +1,40 @@
+package security
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func ValidateToken(ctx *gin.Context, token string) error {
+	claims := &Claims{}
+	jwtSignedKey := []byte("secret-key")
+	tokenParse, err := jwt.ParseWithClaims(token, claims,
+
+		func(token *jwt.Token) (interface{}, error) {
+			return jwtSignedKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			ctx.JSON(http.StatusUnauthorized, err)
+			return err
+		}
+		ctx.JSON(http.StatusBadRequest, err)
+		return err
+	}
+
+	if !tokenParse.Valid {
+		ctx.JSON(http.StatusUnauthorized, "Invalid token")
+		return err
+	}
+
+	ctx.Next()
+	return nil
+}
