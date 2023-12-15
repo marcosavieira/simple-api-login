c66a736a65932def878e6ed350876c53af61e814
devmarcosav@gmail.com <devmarcosav@gmail.com>
2023-12-15 19:21:48 -0300

    feat: create conection to db

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,22 +6,38 @@ import (
 	"os"
 	"testing"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver  = "postgres"
-	dbSourced = "postgresql://root:secret@localhost:5432/simple_login?sslmode=disable"
 )
 
 var testQueries *Queries
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSourced)
+	/* conn, err := sql.Open(dbDriver, dbSourced)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
 	testQueries = New(conn)
+	os.Exit(m.Run()) */
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+	// Recupera valores das variáveis de ambiente
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+
+	// Conecta ao banco de dados
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	// Inicializa a variável global testQueries
+	testQueries = New(conn)
+
+	// Executa os testes
 	os.Exit(m.Run())
 }
